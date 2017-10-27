3e87cd7123c76aeb085458302892c42d4effdbef
Jason <vivanaranja@gmail.com>
2017-10-27 16:42:41 -0700

    formatting and spacing

--- assets/javascript/giphyhw.js
+++ assets/javascript/giphyhw.js
@@ -1,80 +1,71 @@
-// array of outdoorsy type stuff
-var topics = ["yes", "thumbs up", "when", "maybe", "unsure", "no", "sorry", "never", "as+if", "awkard", "just kidding"];
+$(document).ready(function() {
+
+    // array of outdoorsy type stuff
+    var topics = ["yes", "thumbs up", "when", "maybe", "unsure", "no", "sorry", "never", "as if", "awkard", "just kidding"];
 
 
-// display function re-renders the HTML to display the appropriate content
-function displayGifs() {
+    // display function re-renders the HTML to display the appropriate content
+    function displayGifs() {
 
-    var topic = $(this).attr("dataName");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ps3TxsPX4Szs6sxUyhXQtD22q2g2IRVc&limit=10&q=" + topic;
-    // console.log(queryURL);
+        var topic = $(this).attr("dataName");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ps3TxsPX4Szs6sxUyhXQtD22q2g2IRVc&limit=10&q=" + topic;
+        // console.log(queryURL);
 
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .done(function(response) {
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .done(function(response) {
 
-            var results = response.data;
+                var results = response.data;
 
-            for (var i = 0; i < results.length; i++) {
-                var giphyDiv = $("<div class='item'>");
-                // console.log(giphyDiv);
+                for (var i = 0; i < results.length; i++) {
+                    var giphyDiv = $("<div class='item'>");
+                    // console.log(giphyDiv);
 
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height.url);
-                gifImage.attr("style", "height: 200px");
-                gifImage.attr("data-state", "still");
+                    var gifImage = $("<img>");
+                    gifImage.attr("src", results[i].images.fixed_height.url);
+                    gifImage.attr("style", "height: 200px");
+                    gifImage.attr("data-state", "still");
 
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
 
 
-                giphyDiv.prepend(p);
-                giphyDiv.prepend(gifImage);
+                    giphyDiv.prepend(p);
+                    giphyDiv.prepend(gifImage);
 
-                $("#giphyView").prepend(giphyDiv);
+                    $("#giphyView").prepend(giphyDiv);
 
-            }
-        });
+                }
+            });
 
-}
-
-// Function for displaying buttons
-function makeButtons() {
-
-    // Deletes the topics prior to adding new gifs
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#buttonsView").empty();
-    // Loops through the array of topics
-    for (var i = 0; i < topics.length; i++) {
-
-        // Then dynamicaly generates buttons for each gif in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adds a class of topic to our button
-        a.addClass("topic");
-        // Added a data-attribute
-        a.attr("dataName", topics[i]);
-        // Provided the initial button text
-        a.text(topics[i]);
-        // Added the button to the buttons-view div
-        $("#buttonsView").append(a);
     }
-}
+
+    // Function for displaying buttons
+    function makeButtons() {
+
+        // Deletes the topics prior to adding new gifs
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttonsView").empty();
+        // Loops through the array of topics
+        for (var i = 0; i < topics.length; i++) {
+
+            var a = $("<button type='button' class='btn btn-primary btn-sm'>" + " ");
+            // Adds a class of topic to our button
+            a.addClass("topic");
+            // Added a data-attribute
+            a.attr("dataName", topics[i]);
+            // Provided the initial button text
+            a.text(topics[i]);
+            // Added the button to the buttons-view div
+            $("#buttonsView").append(a);
+        }
+    };
 
 
-$("button").on("click", function() {
-    var answer = $(this).attr("data-answer");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        answer + "&api_key=ps3TxsPX4Szs6sxUyhXQtD22q2g2IRVc&limit=10";
-
-    // console.log(answer);
-
-
-
-      // This function handles events where the add ANSWER button is clicked
-      $("#addAnswer").on("click", function(event) {
+    // This function handles events where the addANSWER button is clicked
+    $("#addAnswer").on("click", function(event) {
         event.preventDefault();
         // This line of code will grab the input from the textbox
         var topic = $("#answerMeInput").val().trim();
@@ -85,11 +76,12 @@ $("button").on("click", function() {
         // Calling renderButtons which handles the processing of our ANSWER array
         makeButtons();
 
+    });
+
+
+    // Adding click event listeners to all buttons with a class of "topic"
+    $(document).on("click", ".topic", displayGifs);
+
+    makeButtons();
+
 });
-
-// Adding click event listeners to all elements with a class of "topic"
-$(document).on("click", ".topic", displayGifs);
-
-makeButtons();
-
-
